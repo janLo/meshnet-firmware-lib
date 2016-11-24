7848bd43d2409bb27e1bbdc710c7fe3d96a3ed6c
Jan Losinski <losinski@wh2.tu-dresden.de>
2016-11-24 02:05:59 +0100

    Add config for firmware
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

---
+++ firmware/node_firmware/lib/Config/config.h
@@ -0,0 +1,13 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#define CE_PIN 7
+#define CS_PIN 8
+
+#define PAYLOAD_LEN 32
+#define HASH_LEN 8
+#define session_t uint16_t
+#define SESSION_LEN sizeof(session_t)
+#define MAX_NODES 40;
+
+#endif /* end of include guard: _CONFIG_H_ */
