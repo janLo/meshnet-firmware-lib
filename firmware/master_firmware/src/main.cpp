1b0d29497487a207faf5ac37d54c72f648e533dd
Jan Losinski <losinski@wh2.tu-dresden.de>
2016-11-24 04:23:03 +0100

    implement most basic master proxy
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- firmware/master_firmware/src/main.cpp
+++ firmware/master_firmware/src/main.cpp
@@ -7,7 +7,9 @@
 // Include eeprom.h for AVR (Uno, Nano) etc. except ATTiny
 #include <EEPROM.h>
 
-RF24 radio(7, 8);
+#include "config.h"
+
+RF24 radio(CE_PIN, CS_PIN);
 RF24Network network(radio);
 RF24Mesh mesh(radio, network);
 
@@ -21,7 +23,34 @@ void setup() {
 }
 
 void loop() {
+  char dat[64];
 
   mesh.update();
   mesh.DHCP();
+
+  if (network.available()) {
+    RF24NetworkHeader header;
+    network.peek(header);
+
+    Serial.write(header.from_node);
+    Serial.write(header.type);
+    uint8_t len = network.read(header, &dat, sizeof(dat));
+    Serial.write(len);
+    Serial.write(dat, len);
+    Serial.write('\n');
+  }
+
+  if (Serial.available() > 4) {
+    uint16_t len = Serial.readBytesUntil('\n', dat, sizeof(dat));
+    if (len < 4 && len < 4 + dat[3]) {
+      return;
+    }
+    uint16_t node = dat[1];
+    node |= (dat[0] << 8) & 0xff;
+
+    uint8_t type = dat[2];
+    uint8_t data_len = dat[3];
+
+    mesh.write(dat + 4, type, data_len, node);
+  }
 }
