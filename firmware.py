0221812eac1a54c0f60e881844dcebae2ff2be5c
Jan Losinski <losinski@wh2.tu-dresden.de>
2016-11-24 03:42:13 +0100

    Make firmware script a bit more useful
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- firmware.py
+++ firmware.py
@@ -1,13 +1,17 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
+import os
+import sys
 import argparse
+import subprocess
 
-NODE_LIBS = ["RF24Mesh", "SipHash"]
-MASTER_LIBS = ["RF24Mesh"]
+
+PROJECTS = {"master_firmware": ["RF24Mesh"],
+            "node_firmware": ["RF24Mesh", "SipHash"]}
 
 
 def install(args):
-    pass
+    subprocess.call(["pip", "install", "--upgrade", "platformio"])
 
 
 def init(args):
@@ -15,11 +19,23 @@ def init(args):
 
 
 def libs(args):
-    pass
+    for proj in PROJECTS:
+        print("\nProject: {}".format(proj))
+        if args.action == "install":
+            for lib in PROJECTS[proj]:
+                subprocess.call(["platformio", "lib", "install", lib],
+                        cwd=os.path.join("firmware", proj))
+        else:
+            subprocess.call(["platformio", "lib", "list"],
+                    cwd=os.path.join("firmware", proj))
+        print("\n")
 
 
 def build(args):
-    pass
+    for proj in PROJECTS:
+        print("\nProject: {}".format(proj))
+        subprocess.call(["platformio", "run"],
+                cwd=os.path.join("firmware", proj))
 
 
 def upload(args):
@@ -29,20 +45,21 @@ def upload(args):
 parser = argparse.ArgumentParser(description="handle firmware")
 subparsers = parser.add_subparsers(help="Available commands")
 
-install_parser = subparsers.add_parser("install", help="Install PlatformIO")
-install_parser.set_default(install)
+install_parser = subparsers.add_parser("install", help="Install/Upgrad/Upgradee PlatformIO")
+install_parser.set_defaults(func=install)
 
 init_parser = subparsers.add_parser("init", help="Initialize Projects")
-init_parser.set_default(init)
+init_parser.set_defaults(func=init)
 
 libs_parser = subparsers.add_parser("libs", help="Handle embedded librraies")
-libs_parser.set_default(libs)
+libs_parser.add_argument("action", help="What to do", choices=["list", "install"], default="list")
+libs_parser.set_defaults(func=libs)
 
 build_parser = subparsers.add_parser("build", help="Build firmware")
-build_parser.set_default(build)
+build_parser.set_defaults(func=build)
 
 upload_parser = subparsers.add_parser("upload", help="Upload firmware to controller")
-upload_parser.set_default(upload)
+upload_parser.set_defaults(func=upload)
 
 
 if __name__ == "__main__":
