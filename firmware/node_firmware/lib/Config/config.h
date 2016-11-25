03ea03d7cad22a490f1ebd2a4d0b9ac57c57153e
Jan Losinski <losinski@wh2.tu-dresden.de>
2016-11-25 03:17:25 +0100

    Add more defines
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- firmware/node_firmware/lib/Config/config.h
+++ firmware/node_firmware/lib/Config/config.h
@@ -4,10 +4,17 @@
 #define CE_PIN 7
 #define CS_PIN 8
 
+#define session_t uint16_t
+#define msg_size_t uint8_t
+#define node_t uint16_t
+#define counter_t uint16_t
+#define type_t uint8_t
+
 #define PAYLOAD_LEN 32
 #define HASH_LEN 8
-#define session_t uint16_t
 #define SESSION_LEN sizeof(session_t)
-#define MAX_NODES 40;
+
+#define MAX_MESSAGE_LEN                                                        \
+  PAYLOAD_LEN + HASH_LEN + sizeof(msg_size_t) + sizeof(counter_t);
 
 #endif /* end of include guard: _CONFIG_H_ */
