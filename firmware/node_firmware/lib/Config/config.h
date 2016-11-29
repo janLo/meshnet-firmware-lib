156a9582ed259775ab5796709e08cf8735e7f8e3
Jan Losinski <losinski@wh2.tu-dresden.de>
2016-11-29 03:46:33 +0100

    Put key into config
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- firmware/node_firmware/lib/Config/config.h
+++ firmware/node_firmware/lib/Config/config.h
@@ -24,5 +24,10 @@
 #define CHECK_INTERVAL 1000
 
 #define MASTER_ADDR 0
+#define KEY                                                                    \
+  {                                                                            \
+    0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b,    \
+        0x0c, 0x0d, 0x0e, 0x0f                                                 \
+  }
 
 #endif /* end of include guard: _CONFIG_H_ */
