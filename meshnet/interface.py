935007277afe6012e5127aa4667e7c69f4f2a11a
Jan Losinski <losinski@wh2.tu-dresden.de>
2016-12-06 06:26:59 +0100

    Playground for python service
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

---
+++ meshnet/interface.py
@@ -0,0 +1,27 @@
+import serial
+import struct
+from siphashc import siphash
+
+def _hash(key: str, sender: int, receiver: int, msg_type: int, data: bytes):
+    packed_data = struct.pack(">h>hBs", sender, receiver, msg_type, data)
+    return struct.pack("Q", siphash(key, packed_data))
+
+
+class SerialMessage(object):
+    def __init__(self):
+        pass
+
+    def serialize(self):
+        pass
+
+
+
+
+class Connection(object):
+
+    def __init__(self, device):
+        self._device = device
+        self._conn = None
+
+    def connect(self):
+        self._conn = serial.Serial(self._device, 115200)
