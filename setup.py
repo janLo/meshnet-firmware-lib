0ec81de6f6fe662dc485c7b35188398588e7a332
Jan Losinski <losinski@wh2.tu-dresden.de>
2016-12-29 15:43:33 +0100

    Add a simple setup.py
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='automation_mesh',
+    version='0.1',
+    packages=['meshnet', 'meshnet.serio'],
+    url='https://github.com/janLo/automation_mesh',
+    license='BSD',
+    author='Jan Losinski',
+    author_email='losinskij@gmail.com',
+    description='Create a mesh network for IoT'
+)
