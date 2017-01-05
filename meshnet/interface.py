4445d46e2aed895d1e66c11fbfa5d76a2f8d9ea0
Jan Losinski <losinski@wh2.tu-dresden.de>
2017-01-05 03:41:33 +0100

    Use aio connection in dummy interface
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- meshnet/interface.py
+++ meshnet/interface.py
@@ -1,9 +1,12 @@
+import asyncio
 import logging
-
 import sys
 
-from meshnet.serio.messages import SerialMessageConsumer, SerialMessage, MessageType
-from meshnet.serio.connection import LegacyConnection, MessageHandler, MessageWriter
+import colorlog
+from serial.aio import create_serial_connection
+
+from meshnet.serio.connection import MessageHandler, MessageWriter, AioSerialConnection
+from meshnet.serio.messages import SerialMessage, MessageType
 
 logger = logging.getLogger(__name__)
 
@@ -15,12 +18,15 @@ class TestHandler(MessageHandler):
         self._cnt = 0
         self.key = key
 
+    def next_cnt(self):
+        self._cnt = (self._cnt + 1) % 0xffff
+        return self._cnt
+
     def on_message(self, message: SerialMessage, writer: MessageWriter):
         if not message.verify(self.key):
             logger.warning("cannot verify checksum")
             return
-        self._cnt += 1
-        reply = SerialMessage(0, message.sender, MessageType.configure, None, message.session, self._cnt,
+        reply = SerialMessage(0, message.sender, MessageType.configure, None, message.session, self.next_cnt(),
                               b'\x01\x01h\0')
         writer.put_packet(reply, self.key)
 
@@ -32,12 +38,19 @@ class TestHandler(MessageHandler):
 
 
 if __name__ == "__main__":
-    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+    handler = colorlog.StreamHandler()
+    handler.setFormatter(colorlog.ColoredFormatter(
+        '%(log_color)s%(levelname)s:%(name)s:%(message)s'))
+
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG, handlers=[handler])
 
     handler = TestHandler(KEY)
-    conn = LegacyConnection(sys.argv[1])
+
+    conn = AioSerialConnection()
     conn.register_handler(handler)
 
-    conn.connect()
-    while True:
-        conn.read()
+    loop = asyncio.get_event_loop()
+    coro = create_serial_connection(loop, conn, sys.argv[1], baudrate=115200)
+    loop.run_until_complete(coro)
+    loop.run_forever()
+    loop.close()
