7c62e78c006cf5599f8d5a2fb2ee1eac2518cc0b
Jan Losinski <losinski@wh2.tu-dresden.de>
2017-01-19 00:22:06 +0100

    switch setup.py to setuptools
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='automation_mesh',
     version='0.1',
-    packages=['meshnet', 'meshnet.serio'],
+    packages=find_packages(".", exclude=["tests"]),
     url='https://github.com/janLo/automation_mesh',
     license='BSD',
     author='Jan Losinski',
     author_email='losinskij@gmail.com',
-    description='Create a mesh network for IoT'
+    description='Create a mesh network for IoT',
+    install_requires=[
+        "pySerial",
+        "pyserial-asyncio",
+        "siphashc3",
+        "colorlog",
+        "voluptuous",
+        "pyyaml",
+    ]
 )
